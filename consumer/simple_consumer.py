358dba82c5cdedaf1e314e54bc6bf71addfe60a5
Ahnaf19 <ahnaftanjid19@gmail.com>
2025-07-26 12:33:00 +0600

    init consumer

--- consumer/simple_consumer.py
+++ consumer/simple_consumer.py
@@ -1,1 +1,22 @@
 from quixstreams import Application
+
+app = Application(
+    broker_address="docker.host.internal:9092",
+    loglevel="DEBUG",
+    # consumer group for 
+    consumer_group="weather_reader"
+)
+
+"""
+can use breakpoint() inside the while loop, to see what's going on use commands like:
+ - list
+ - pp locals()
+"""
+
+with app.get_consumer() as consumer:
+    consumer.subscribe(["weather_data_demo"])
+    
+    while True:
+        consumer.poll(timeout=1)
+        # if msg is None:
+        breakpoint()
